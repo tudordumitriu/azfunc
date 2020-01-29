bf3bf0a3750e6c892a27046de6d046386666ffa7
Tudor Dumitriu <tudor.dumitriu@adaptabi.com>
2020-01-29 21:21:52 +0200

    Build test

--- WiteToBlobFunction.cs
+++ WiteToBlobFunction.cs
@@ -20,10 +20,10 @@ namespace AZEFunctions
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            fileStorage.WriteLine($"Hello from {input.Name} updated");
+            fileStorage.WriteLine($"Hello from {input.Name} za slick updated");
 
             return input.Name != null
-                ? (ActionResult)new OkObjectResult($"Hello, {input.Name}")
+                ? (ActionResult)new OkObjectResult($"Hello, {input.Name} updated")
                 : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
         }
 
